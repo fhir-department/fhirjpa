b6bffabea67eb004f98ce762641ccdad28ec01ce
henrym6003 <hgmosel@gmail.com>
2019-01-23 11:14:24 -0700

    moar deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 echo Step 1/4 Build WAR file
-mvn package && \
-  docker build -t claimcdr/fhirjpa-java .
+    mvn clean install;
 
 
-docker push claimcdr/fhirjpa-java:latest;
-
-#echo Step 2/4: Creating new production image;
-#  mv Dockerfile.prod.off Dockerfile;
-#    docker build -t claimcdr/fhirjpa-java . ;
-#  mv Dockerfile Dockerfile.prod.off;
+echo Step 2/4: Creating new production image;
+  mv Dockerfile.prod.off Dockerfile;
+    docker build -t claimcdr/fhirjpa-java . ;
+  mv Dockerfile Dockerfile.prod.off;
 
 
-#echo Step 3/4: Push image to Docker Hub repo;
-#  docker push claimcdr/fhirjpa-java:latest;
-#
-#
-#echo Step 4/4: Deploy to AWS
-#  eb deploy
+echo Step 3/4: Push image to Docker Hub repo;
+  docker push claimcdr/fhirjpa-java:latest;
 
+
+echo Step 4/4: Deploy to AWS
+  eb deploy
